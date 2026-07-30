09bd8b7ff9c82f51c58ace19e030d5e3eaec8433
Mia Monkovic <MMonkovic@mathematica-mpr.com>
2026-07-30 19:06:23 -0400

    feat: firebase codes

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME",
+  apiKey: "AIzaSyBCmMrqGP0g8uvmsWM1EFVJnzrGIeSqTK4",
+  authDomain: "friend-tracker-2928e.firebaseapp.com",
+  projectId: "friend-tracker-2928e",
+  storageBucket: "friend-tracker-2928e.firebasestorage.app",
+  messagingSenderId: "664648652835",
+  appId: "1:664648652835:web:bfc0ead1bd24387b6d3996",
 };
